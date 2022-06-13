6b5471da517f91b722bb8927a9697734823f1592
datdo2112001 <62889310+datdo2112001@users.noreply.github.com>
2022-06-13 09:24:06 -0700

    Update notifications_channel.js

--- app/javascript/channels/notifications_channel.js
+++ app/javascript/channels/notifications_channel.js
@@ -3,7 +3,7 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("NotificationsChannel", {
   connected() {
-    console.log("Connected!");
+    console.log("Connected!")
     // Called when the subscription is ready for use on the server
   },
 
